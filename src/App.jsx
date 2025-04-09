5c85e500928c2234e00a4627fdfb7fc3deffc7d5
lorenzosalti <lorenzosalti@gmail.com>
2025-04-09 16:05:36 +0200

    fea: add form, state variable, submit function

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-
+import { useState } from "react"
 
 const articles = ['Articolo 1', 'Articolo 2', 'Articolo 3', 'Articolo 4']
 
 
 function App() {
 
+  const [newArticle, setNewArticle] = useState('')
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    console.log('submit')
+  }
+
   return (
     <>
       <h1>Nome del Blog</h1>
 
       {articles.map(article => <div>{article}</div>)}
+
+      <hr />
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={newArticle} onChange={e => setNewArticle(e.target.value)} />
+        <button type="submit">Inserisci Articolo</button>
+      </form>
     </>
   )
 }
