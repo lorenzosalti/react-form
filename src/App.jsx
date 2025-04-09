a62f29915f35f477b5f95a8ea815567c9490bd60
lorenzosalti <lorenzosalti@gmail.com>
2025-04-09 15:40:31 +0200

    feat: articles array iteration displayed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
 
+const articles = ['Articolo 1', 'Articolo 2', 'Articolo 3', 'Articolo 4']
 
 
 function App() {
 
   return (
     <>
+      <h1>Nome del Blog</h1>
 
+      {articles.map(article => <div>{article}</div>)}
     </>
   )
 }
